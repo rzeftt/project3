1224e14f87b6eb6d4c2f46ae6caf37872a6dc293
rzeftt <164239172+rzeftt@users.noreply.github.com>
2024-12-22 19:07:44 +0200

    Update appConfig.ts

--- project-be/src/utils/appConfig.ts
+++ project-be/src/utils/appConfig.ts
@@ -1,7 +1,7 @@
 class AppConfig {
     readonly port: number = 4091;
     readonly dbConfig = {
-        user: 'root',
+        user: 'admin',
        host: 'freedom-db.c56gwmwoeix6.us-east-1.rds.amazonaws.com',
         port: 3306,
         password: 'rzf1024+_',
