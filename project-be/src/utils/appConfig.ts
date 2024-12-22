30a329a5fc1a565f66a380f25a355757694e9a18
rzeftt <164239172+rzeftt@users.noreply.github.com>
2024-12-22 15:08:10 +0200

    Update appConfig.ts

--- project-be/src/utils/appConfig.ts
+++ project-be/src/utils/appConfig.ts
@@ -2,7 +2,7 @@ class AppConfig {
     readonly port: number = 4091;
     readonly dbConfig = {
         user: 'root',
-        host: 'localhost',
+       host: '54.234.99.29',
         port: 3306,
         password: '',
         database: 'freedom',
